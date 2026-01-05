13bcc34e278d3f47c66fccf15bba84860b564ec5
Moh Aarif Ahmed <34788778+arifamd00@users.noreply.github.com>
2026-01-05 17:28:38 +0000

    updated settings.py and urls.py

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,14 @@ SECRET_KEY = 'django-insecure-%l-m$1bjqrjk2k8pr488z76i1(t6u)fr&dcjdr=jq1)hhwgl+&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME', '')
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    f'{CODESPACE_NAME}-8000.app.github.dev' if CODESPACE_NAME else '*',
+    '*',
+]
 
 
 # Application definition
